1f3296b0a4a3450d8fc624dad65ed67b364dcae3
Syed Shah <195543322+Syed930s@users.noreply.github.com>
2026-09-18 22:10:50 +0100

    Add files via upload

---
+++ SophisCAT.py
@@ -0,0 +1,47 @@
+import sys
+import base64
+import os
+import random
+import string
+
+if len(sys.argv) != 2:
+    print("usage: python3 SophisCAT.py <file>")
+    sys.exit(1)
+
+infile = sys.argv[1]
+if not os.path.isfile(infile):
+    print("file not found")
+    sys.exit(1)
+
+with open(infile, "rb") as f:
+    data = f.read()
+
+b64 = base64.b64encode(data).decode("ascii")
+basename = os.path.splitext(os.path.basename(infile))[0]
+outfile = basename + ".py"
+orig_name = os.path.basename(infile)
+
+print('Include subprocess.Popen([sys.executable, "' + outfile + '"]) in the generated code? (y/n)')
+ans = input().strip().lower()
+include_pop = ans in ("y", "yes")
+
+def rand_var():
+    return "".join(random.choices(string.ascii_letters, k=random.randint(6, 12)))
+
+v1 = rand_var()
+v2 = rand_var()
+while v2 == v1:
+    v2 = rand_var()
+
+code = "import base64, subprocess, sys, os\n"
+code += v1 + ' = "' + b64 + '"\n'
+code += v2 + " = base64.b64decode(" + v1 + ")\n"
+code += 'with open("' + orig_name + '","wb") as f:\n'
+code += "    f.write(" + v2 + ")\n"
+if include_pop:
+    code += 'subprocess.Popen([sys.executable, "' + outfile + '"])\n'
+
+with open(outfile, "w") as f:
+    f.write(code)
+
+print("Created " + outfile)
